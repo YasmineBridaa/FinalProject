302ddc724d116e698d6f8d92cacd404e64694c39
Yasmine Bridaa <yasminebridaa1@gmail.com>
2023-06-27 14:38:26 -0400

    Final Project

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Color(.systemGray)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Text("The Impact Map")
+                
+                Spacer()
+                
+                Text("Up-to-date, reliab")
+                
+            }
+            
         }
-        .padding()
+        
     }
 }
 
